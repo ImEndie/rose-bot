9003cdc9f11827baaff40d31b2749ff0e1a5c45a
Endie <110929183+ImEndie@users.noreply.github.com>
2023-06-05 22:36:10 +0500

    Create chat.py

---
+++ chat.py
@@ -0,0 +1,31 @@
+import openai
+
+openai.api_key = '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'  # Replace with your OpenAI API key
+
+history=[
+            {"role": "system", "content": "Hi. Your name is Ro'za and you are created by a programmer called Endie to help people."},
+            {"role": "assistant", "content": "OK."},
+        ]
+
+def req(message):
+    otm=history.copy()
+    otm.append({'role':'user','content':message})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=otm
+    )
+    if response['choices'][0]['message']['content']:
+        history.append(otm[-1])
+        history.append(response['choices'][0]['message'])
+        return response['choices'][0]['message']['content']
+    else:
+        return "Kechirasiz, savolingizni tushunmadim"
+
+def gen_img(t):
+    response = openai.Image.create(
+        prompt=t,
+        n=1,
+        size="1024x1024"
+    )
+    image_url = response['data'][0]['url']
+    return image_url
